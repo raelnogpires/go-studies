750d5438362f3a1879773d313b391eb96ccf76d4
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-26 15:17:12 -0300

    feat: exercise1 complete

---
+++ exercises-1-variables-values-and-types/exercise1.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// Utilizando o operador curto de declaração,
+// atribua estes valores às variáveis com os identificadores "x", "y", e "z".
+// I. 42, II. "James Bond", III. true
+
+// Demonstre os valores dessas variáveis usando uma única declaração de print
+// e também múltiplas declarações
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+
+	fmt.Println("x:", x)
+	fmt.Println("y:", y)
+	fmt.Println("z:", z)
+}
