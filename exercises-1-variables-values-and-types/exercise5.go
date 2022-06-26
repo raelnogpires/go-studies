0381c0ddc200c798385415de755f9a04580ce04e
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-26 16:32:55 -0300

    feat: exercise5 complete

---
+++ exercises-1-variables-values-and-types/exercise5.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// Em package-level scope, utilizando a palavra-chave var, crie uma variável com
+// o identificador "y". O tipo desta variável deve ser o tipo subjacente do tipo que você
+// criou no exercício anterior.
+
+var y int
+
+type something int
+
+var x something
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("x: %T\n", x)
+
+	x = 10
+	fmt.Println(x)
+
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("y: %T\n", y)
+}
