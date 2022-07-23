27b2b4042b87ca5462b2ae3bd51b6a1ad584bfb6
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-07-23 15:08:58 -0300

    module2: exercise6 complete

---
+++ exercises-2-programming-fundamentals/exercise6.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// using iota, create 4 constants which the values are the 4 next years
+// print the results
+
+const (
+	year1 = 2023 + iota
+	year2
+	year3
+	year4
+)
+
+func main() {
+	fmt.Println(year1, year2, year3, year4)
+}
