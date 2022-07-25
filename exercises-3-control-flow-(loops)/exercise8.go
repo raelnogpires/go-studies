90cd9194babffce0f0e0b22ea827e741b4a41e25
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-07-25 20:06:40 -0300

    module3: exercise8 complete

---
+++ exercises-3-control-flow-(loops)/exercise8.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// create a program that use switch statement where the switch are
+// a string variable with the identifier `favSport`
+
+func main() {
+	favSport := "soccer"
+
+	switch favSport {
+	case "soccer":
+		fmt.Println("flamengo <3")
+		break
+	case "volley":
+		fmt.Println("bruninho and wallace are unstoppable")
+		break
+	case "basketball":
+		fmt.Println("have you heard about that stephen curry guy?")
+		break
+	default:
+		fmt.Println("I don't know that sport!")
+		break
+	}
+}
