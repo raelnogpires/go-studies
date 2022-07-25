11a1f5fcf4a71f502bce50a18ed59222f6c8aac3
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-07-25 20:10:43 -0300

    module3: exercise9 complete

---
+++ exercises-3-control-flow-(loops)/exercise9.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// write by hand the result of the following:
+
+func main() {
+	// fmt.Println(true && true)  // logs true
+	fmt.Println(true && false) // logs false
+	// fmt.Println(true || true)  // logs true
+	fmt.Println(true || false) // logs true
+	fmt.Println(!true)         // logs false
+}
+
+// line 8 and 10 commented bc redudant error
